f3fbe5def8ce5f29ab4bcdb84b5683ebb67d0893
lukke <lukkeyn33@gmail.com>
2024-04-24 12:17:36 +0300

    update task4

--- task4/Task4.java
+++ task4/Task4.java
@@ -7,21 +7,25 @@ public class Task4 {
     public static void main(String[] args) {
         List<Integer> arrayList = new ArrayList<>();
         int count = 0;
-        while (count < args.length) { // заполняем  данными из файла
-            for (String arg : args
-            ) {
-                fillCollection(arg, arrayList);
+        if (args.length < 1) {
+            System.out.println("Неверное количество аргументов"); // проверяем количество аргументов
+        } else {
+            while (count < args.length) { // заполняем  данными из файла
+                for (String arg : args
+                ) {
+                    fillCollection(arg, arrayList);
+                }
+                count++;
             }
-            count++;
-        }
-        int result = findMinSteps(arrayList.get(0), arrayList); // присваиваем значение первого элемента, с которым будем сравнивать все остальные вариации
-        for (int i = 1; i < arrayList.size(); i++) {
-            int tempResult = findMinSteps(arrayList.get(i), arrayList);// пишем в переменную результат шагов каждой итерации
-            if (tempResult < result) { // если находим меньшее число шагов, чем при приведении к первому числу - делаем его минимальным результатом
-                result = tempResult;
+            int result = findMinSteps(arrayList.get(0), arrayList); // присваиваем значение первого элемента, с которым будем сравнивать все остальные итерации
+            for (int i = 1; i < arrayList.size(); i++) {
+                int tempResult = findMinSteps(arrayList.get(i), arrayList);// пишем в переменную результат шагов каждой итерации
+                if (tempResult < result) { // если находим меньшее число шагов, чем при приведении к первому числу - делаем его минимальным результатом
+                    result = tempResult;
+                }
             }
+            System.out.println(result);
         }
-        System.out.println(result);
     }
 
     public static int findMinSteps(int number, List<Integer> arrayList) {
